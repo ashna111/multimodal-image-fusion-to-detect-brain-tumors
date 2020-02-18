9ce21bd42d3c76f037e87312e190c85378687e0b
ashna111 <ashnashah111@gmail.com>
2020-02-18 16:03:14 +0530

    file upload using flask

---
+++ app.py
@@ -0,0 +1,26 @@
+import os
+from datetime import datetime
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+@app.route("/")
+def index():
+    return render_template("upload.html")
+
+@app.route("/upload", methods=['POST'])
+def upload():
+    target = os.path.join(APP_ROOT, 'uploads/')
+    if not os.path.isdir(target):
+        os.mkdir(target)
+
+    for file in request.files.getlist("file"):
+        filename = file.filename
+        destination = "/".join([target, filename])
+        file.save(destination)
+
+    return render_template("complete.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
