ce6544fb23cc958af686c2092ac3f328bf662604
Rebecca Biju <rebecca.biju@somaiya.edu>
2020-02-26 00:56:18 +0530

    Image updation fix

--- app.py
+++ app.py
@@ -24,7 +24,15 @@ def upload():
     destination2 = "/".join([target, "ct.jpg"])
     ct_file.save(destination2)
     
+
     return render_template("registration.html")
+    
+@app.after_request
+def add_header(response):
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
+    response.headers['Expires'] = '0'
+    return response
 
 if __name__ == "__main__":
     app.run(debug=True)
