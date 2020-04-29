1eeaa5570efd17a0163e6b5ee878a0d14cb1a212
ashna111 <ashnashah111@gmail.com>
2020-04-29 09:37:38 +0530

    update fusion process on flask

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ import matplotlib.cm as cm
 import cv2
 import imageio
 import scipy.ndimage as ndi
+import argparse
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torchvision.models.vgg import vgg19
+import pywt
+import pywt.data
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -23,48 +30,6 @@ def convertToIntList(arr):
     return result
 
 def procrustes(X, Y, scaling=True, reflection='best'):
-    """
-    A port of MATLAB's `procrustes` function to Numpy.
-
-    Procrustes analysis determines a linear transformation (translation,
-    reflection, orthogonal rotation and scaling) of the points in Y to best
-    conform them to the points in matrix X, using the sum of squared errors
-    as the goodness of fit criterion.
-
-        d, Z, [tform] = procrustes(X, Y)
-
-    Inputs:
-    ------------
-    X, Y    
-        matrices of target and input coordinates. they must have equal
-        numbers of  points (rows), but Y may have fewer dimensions
-        (columns) than X.
-
-    scaling 
-        if False, the scaling component of the transformation is forced
-        to 1
-
-    reflection
-        if 'best' (default), the transformation solution may or may not
-        include a reflection component, depending on which fits the data
-        best. setting reflection to True or False forces a solution with
-        reflection or no reflection respectively.
-
-    Outputs
-    ------------
-    d       
-        the residual sum of squared errors, normalized according to a
-        measure of the scale of X, ((X - X.mean(0))**2).sum()
-
-    Z
-        the matrix of transformed Y-values
-
-    tform   
-        a dict specifying the rotation, translation and scaling that
-        maps X --> Y
-
-    """
-
     n,m = X.shape
     ny,my = Y.shape
 
@@ -134,6 +99,150 @@ def procrustes(X, Y, scaling=True, reflection='best'):
 
     return d, Z, tform
 
+# VGG19 CNN For Fusion
+class VGG19(torch.nn.Module):
+    def __init__(self, device='cpu'):
+        super(VGG19, self).__init__()
+        features = list(vgg19(pretrained=True).features)
+        if device == "cuda":
+            self.features = nn.ModuleList(features).cuda().eval()
+        else:
+            self.features = nn.ModuleList(features).eval()
+
+    def forward(self, x):
+        feature_maps = []
+        for idx, layer in enumerate(self.features):
+            x = layer(x)
+            if idx == 3:
+                feature_maps.append(x)
+        return feature_maps
+
+class Fusion:
+    def __init__(self, input):
+        """
+        Class Fusion constructor
+
+        Instance Variables:
+            self.images: input images
+            self.model: CNN model, default=vgg19
+            self.device: either 'cuda' or 'cpu'
+        """
+        self.input_images = input
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model = VGG19(self.device)
+
+    def fuse(self):
+        """
+        A top level method which fuse self.images
+        """
+        # Convert all images to YCbCr format
+        self.normalized_images = [-1 for img in self.input_images]
+        self.YCbCr_images = [-1 for img in self.input_images]
+        for idx, img in enumerate(self.input_images):
+            if not self._is_gray(img):
+                self.YCbCr_images[idx] = self._RGB_to_YCbCr(img)
+                self.normalized_images[idx] = self.YCbCr_images[idx][:, :, 0]
+            else:
+                self.normalized_images[idx] = img / 255.
+        # Transfer all images to PyTorch tensors
+        self._tranfer_to_tensor()
+        # Perform fuse strategy
+        fused_img = self._fuse()[:, :, 0]
+        # Reconstruct fused image given rgb input images
+        for idx, img in enumerate(self.input_images):
+            if not self._is_gray(img):
+                self.YCbCr_images[idx][:, :, 0] = fused_img
+                fused_img = self._YCbCr_to_RGB(self.YCbCr_images[idx])
+                fused_img = np.clip(fused_img, 0, 1)
+
+        return (fused_img * 255).astype(np.uint8)
+        # return fused_img
+
+    def _fuse(self):
+        """
+        Perform fusion algorithm
+        """
+        with torch.no_grad():
+
+            imgs_sum_maps = [-1 for tensor_img in self.images_to_tensors]
+            for idx, tensor_img in enumerate(self.images_to_tensors):
+                imgs_sum_maps[idx] = []
+                feature_maps = self.model(tensor_img)
+                for feature_map in feature_maps:
+                    sum_map = torch.sum(feature_map, dim=1, keepdim=True)
+                    imgs_sum_maps[idx].append(sum_map)
+
+            max_fusion = None
+            for sum_maps in zip(*imgs_sum_maps):
+                features = torch.cat(sum_maps, dim=1)
+                weights = self._softmax(F.interpolate(features,
+                                        size=self.images_to_tensors[0].shape[2:]))
+                weights = F.interpolate(weights,
+                                        size=self.images_to_tensors[0].shape[2:])
+                current_fusion = torch.zeros(self.images_to_tensors[0].shape)
+                for idx, tensor_img in enumerate(self.images_to_tensors):
+                    current_fusion += tensor_img * weights[:,idx]
+                if max_fusion is None:
+                    max_fusion = current_fusion
+                else:
+                    max_fusion = torch.max(max_fusion, current_fusion)
+
+            output = np.squeeze(max_fusion.cpu().numpy())
+            if output.ndim == 3:
+                output = np.transpose(output, (1, 2, 0))
+            return output
+        
+        
+    def _RGB_to_YCbCr(self, img_RGB):
+            """
+            A private method which converts an RGB image to YCrCb format
+            """
+            img_RGB = img_RGB.astype(np.float32) / 255.
+            return cv2.cvtColor(img_RGB, cv2.COLOR_RGB2YCrCb)
+
+    def _YCbCr_to_RGB(self, img_YCbCr):
+            """
+            A private method which converts a YCrCb image to RGB format
+            """
+            img_YCbCr = img_YCbCr.astype(np.float32)
+            return cv2.cvtColor(img_YCbCr, cv2.COLOR_YCrCb2RGB)
+
+    def _is_gray(self, img):
+            """
+            A private method which returns True if image is gray, otherwise False
+            """
+            if len(img.shape) < 3:
+                return True
+            if img.shape[2] == 1:
+                return True
+            b, g, r = img[:,:,0], img[:,:,1], img[:,:,2]
+            if (b == g).all() and (b == r).all():
+                return True
+            return False
+
+    def _softmax(self, tensor):
+            """
+            A private method which compute softmax ouput of a given tensor
+            """
+            tensor = torch.exp(tensor)
+            tensor = tensor / tensor.sum(dim=1, keepdim=True)
+            return tensor
+
+    def _tranfer_to_tensor(self):
+            """
+            A private method to transfer all input images to PyTorch tensors
+            """
+            self.images_to_tensors = []
+            for image in self.normalized_images:
+                np_input = image.astype(np.float32)
+                if np_input.ndim == 2:
+                    np_input = np.repeat(np_input[None, None], 3, axis=1)
+                else:
+                    np_input = np.transpose(np_input, (2, 0, 1))[None]
+                if self.device == "cuda":
+                    self.images_to_tensors.append(torch.from_numpy(np_input).cuda())
+                else:
+                    self.images_to_tensors.append(torch.from_numpy(np_input)) 
 
 # Routes
 @app.route("/")
@@ -191,12 +300,106 @@ def register():
 def registerimage():
     return render_template("imageregistration.html")
 
+@app.route("/fusion")
+def fusion():
+    # Load MRI image
+    mri_image = cv2.imread('static/mri_registered.jpg')
+    mri_image = cv2.cvtColor(mri_image, cv2.COLOR_BGR2GRAY)
+
+    # Wavelet transform of image, and plot approximation and details
+    coeffs2 = pywt.dwt2(mri_image, 'haar')
+    LL, (LH, HL, HH) = coeffs2
+    for i, a in enumerate([LL, LH, HL, HH]):
+        path='static/mri_'+str(i)+'.jpg'
+        cv2.imwrite(path,a)
+
+    # Load CT Image
+    ct_image = cv2.imread('static/ct.jpg')
+    ct_image = cv2.cvtColor(ct_image, cv2.COLOR_BGR2GRAY)
+
+    # Wavelet transform of image, and plot approximation and details
+    coeffs2 = pywt.dwt2(ct_image, 'haar')
+    LL, (LH, HL, HH) = coeffs2
+
+    for i, a in enumerate([LL, LH, HL, HH]):
+        path='static/ct_'+str(i)+'.jpg'
+        cv2.imwrite(path,a)
+
+    input_images=[]
+    mri = cv2.imread('static/mri_0.jpg')
+    mri = cv2.cvtColor(mri, cv2.COLOR_BGR2GRAY)
+
+    ct = cv2.imread('static/ct_0.jpg')
+    ct = cv2.cvtColor(ct, cv2.COLOR_BGR2GRAY)
+    input_images.append(mri)
+    input_images.append(ct)
+    FU = Fusion(input_images)
+    fusion_img = FU.fuse()
+    cv2.imwrite('static/fusion_0.jpg', fusion_img)
+
+    input_images=[]
+    mri = cv2.imread('static/mri_1.jpg')
+    mri = cv2.cvtColor(mri, cv2.COLOR_BGR2GRAY)
+
+    ct = cv2.imread('static/ct_1.jpg')
+    ct = cv2.cvtColor(ct, cv2.COLOR_BGR2GRAY)
+    input_images.append(mri)
+    input_images.append(ct)
+    FU = Fusion(input_images)
+    fusion_img = FU.fuse()
+    cv2.imwrite('static/fusion_1.jpg', fusion_img)
+
+    input_images=[]
+    mri = cv2.imread('static/mri_2.jpg')
+    mri = cv2.cvtColor(mri, cv2.COLOR_BGR2GRAY)
+
+    ct = cv2.imread('static/ct_2.jpg')
+    ct = cv2.cvtColor(ct, cv2.COLOR_BGR2GRAY)
+    input_images.append(mri)
+    input_images.append(ct)
+    FU = Fusion(input_images)
+    fusion_img = FU.fuse()
+    cv2.imwrite('static/fusion_2.jpg', fusion_img)
+
+    input_images=[]
+    mri = cv2.imread('static/mri_3.jpg')
+    mri = cv2.cvtColor(mri, cv2.COLOR_BGR2GRAY)
+
+    ct = cv2.imread('static/ct_3.jpg')
+    ct = cv2.cvtColor(ct, cv2.COLOR_BGR2GRAY)
+    input_images.append(mri)
+    input_images.append(ct)
+    FU = Fusion(input_images)
+    fusion_img = FU.fuse()
+    cv2.imwrite('static/fusion_3.jpg', fusion_img)
+    
+    fusion_0 = cv2.imread('static/fusion_0.jpg')
+    fusion_0 = cv2.cvtColor(fusion_0, cv2.COLOR_BGR2GRAY)
+
+    fusion_1 = cv2.imread('static/fusion_1.jpg')
+    fusion_1 = cv2.cvtColor(fusion_1, cv2.COLOR_BGR2GRAY)
+
+    fusion_2 = cv2.imread('static/fusion_2.jpg')
+    fusion_2 = cv2.cvtColor(fusion_2, cv2.COLOR_BGR2GRAY)
+
+    fusion_3 = cv2.imread('static/fusion_3.jpg')
+    fusion_3 = cv2.cvtColor(fusion_3, cv2.COLOR_BGR2GRAY)
+
+    coeffs=(fusion_0,(fusion_1,fusion_2,fusion_3))
+    fusion=pywt.idwt2(coeffs,'haar')
+    cv2.imwrite('static/fusion.jpg',fusion)
+
+    return render_template("fusion.html")
+
+
 @app.after_request
 def add_header(response):
     response.headers['Pragma'] = 'no-cache'
     response.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate'
     response.headers['Expires'] = '0'
     return response
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
