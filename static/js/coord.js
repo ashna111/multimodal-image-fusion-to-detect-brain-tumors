079395b1c72f7f23abf9c7b14ebcf96d41230ae1
ashna111 <ashnashah111@gmail.com>
2020-05-03 09:56:09 +0530

    css update

--- static/js/coord.js
+++ static/js/coord.js
@@ -13,41 +13,8 @@ function FindPosition(oElement) {
     }
 }
 
-// function ChangeColour(PosX, PosY, myImg){
-//     console.log("In Change Colour");
-//     var canvas = document.createElement('canvas');
-//     var context = canvas.getContext('2d');
-//     var img = document.getElementById('mri');
-//     // canvas.width = img.width;
-//     // canvas.height = img.height;
-//     context.drawImage(img, 0, 0 );
-//     var imgData = context.getImageData(0, 0, img.width, img.height);
-
-//     var i;
-//     for (i = 0; i < imgData.data.length; i += 4) {
-//     imgData.data[i] = 255-imgData.data[i];
-//     imgData.data[i + 1] = 255-imgData.data[i + 1];
-//     imgData.data[i + 2] = 255-imgData.data[i + 2];
-//     imgData.data[i + 3] = 255;
-//     }
-//     console.log(imgData);
-//     // imgData.data[0]=255;
-//     // imgData.data[1]=0;
-//     // imgData.data[2]=0;
-//     // imgData.data[3]=255;
-// }
-
 function GetCoordinatesMri(e) {
     if (mriCoord.length < points) {
-        // console.log("Making Canvas")
-        // var canvas = document.createElement('canvas');
-        // var context = canvas.getContext('2d');
-        // var img = document.getElementById('mri');
-        // canvas.width = img.width;
-        // canvas.height = img.height;
-        // context.drawImage(img, 0, 0);
-
-        console.log(myImgMri)
         var PosX = 0;
         var PosY = 0;
         var ImgPos;
@@ -71,37 +38,6 @@ function GetCoordinatesMri(e) {
 
         var imgData = context.getImageData(PosX, PosY, img.width, img.height);
         var data = imgData.data;
-
-        // for (var y = 0; y < img.height; y++) {
-        //     for (var x = 0; x < img.width; x++) {
-        //         var index = (x + img.width * y) * 4;
-        //         data[index+0] = data[index+2];
-        //         data[index+1] = 255 - data[index+1];
-        //         data[index+2] = 255 - data[index-1];
-
-        //     }
-        // }
-
-        // var x = 20;
-        // var y = 20;
-        // data[((img.width * PosY) + PosX) * 4]=255;
-        // data[((img.width * PosY) + PosX) * 4 + 1]=0;
-        // data[((img.width * PosY) + PosX) * 4 + 2]=0;
-        // var alpha = data[((img.width * PosY) + PosX) * 4 + 3];
-        // // console.log(red);
-        // console.log(data[((img.width * PosY) + PosX) * 4])
-
-
-        // var i;
-        // for (i = 0; i < imgData.data.length; i += 4) {
-        // imgData.data[i] = 255-imgData.data[i];
-        // imgData.data[i + 1] = 255-imgData.data[i + 1];
-        // imgData.data[i + 2] = 255-imgData.data[i + 2];
-        // imgData.data[i + 3] = 255;
-        // }
-        // console.log(imgData);
-
-        // ChangeColour(PosX, PosY, myImg);
     } else {
         alert("Can't exceed number of points");
     }
@@ -110,14 +46,6 @@ function GetCoordinatesMri(e) {
 function GetCoordinatesCt(e) {
 
     if (ctCoord.length < points) {
-        // console.log("Making Canvas")
-        // var canvas = document.createElement('canvas');
-        // var context = canvas.getContext('2d');
-        // var img = document.getElementById('ct');
-        // canvas.width = img.width;
-        // canvas.height = img.height;
-        // context.drawImage(img, 0, 0);
-
         console.log(myImgCt)
         var PosX = 0;
         var PosY = 0;
@@ -144,37 +72,6 @@ function GetCoordinatesCt(e) {
 
         var imgData = context.getImageData(PosX, PosY, img.width, img.height);
         var data = imgData.data;
-
-        // for (var y = 0; y < img.height; y++) {
-        //     for (var x = 0; x < img.width; x++) {
-        //         var index = (x + img.width * y) * 4;
-        //         data[index+0] = data[index+2];
-        //         data[index+1] = 255 - data[index+1];
-        //         data[index+2] = 255 - data[index-1];
-
-        //     }
-        // }
-
-        // var x = 20;
-        // var y = 20;
-        // data[((img.width * PosY) + PosX) * 4]=255;
-        // data[((img.width * PosY) + PosX) * 4 + 1]=0;
-        // data[((img.width * PosY) + PosX) * 4 + 2]=0;
-        // var alpha = data[((img.width * PosY) + PosX) * 4 + 3];
-        // // console.log(red);
-        // console.log(data[((img.width * PosY) + PosX) * 4])
-
-
-        // var i;
-        // for (i = 0; i < imgData.data.length; i += 4) {
-        // imgData.data[i] = 255-imgData.data[i];
-        // imgData.data[i + 1] = 255-imgData.data[i + 1];
-        // imgData.data[i + 2] = 255-imgData.data[i + 2];
-        // imgData.data[i + 3] = 255;
-        // }
-        // console.log(imgData);
-
-        // ChangeColour(PosX, PosY, myImg);
     } else {
         alert("Can't exceed number of points");
     }
