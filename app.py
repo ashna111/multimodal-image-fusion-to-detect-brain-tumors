34ac5533930b49ee3cf9b82b9d68f1de1ca7ca23
ashna111 <ashnashah111@gmail.com>
2020-06-18 19:55:57 +0530

    update flask app

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ import torch.nn.functional as F
 from torchvision.models.vgg import vgg19
 import pywt
 import pywt.data
+from skimage.morphology import extrema
+from skimage.morphology import watershed as skwater
 
 app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
@@ -390,6 +392,56 @@ def fusion():
     cv2.imwrite('static/fusion.jpg',fusion)
 
     return render_template("fusion.html")
+
+@app.route("/segmentation")
+def segmentation():
+    img = cv2.imread("static/fusion.jpg")
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_OTSU)
+
+    ret, markers = cv2.connectedComponents(thresh)
+
+    marker_area = [np.sum(markers==m) for m in range(np.max(markers)) if m!=0] 
+    largest_component = np.argmax(marker_area)+1 #Add 1 since we dropped zero above                        
+    brain_mask = markers==largest_component
+
+    brain_out = img.copy()
+    brain_out[brain_mask==False] = (0,0,0)
+
+    img = cv2.imread("static/fusion.jpg")
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
+
+    # noise removal
+    kernel = np.ones((3,3),np.uint8)
+    opening = cv2.morphologyEx(thresh,cv2.MORPH_OPEN,kernel, iterations = 2)
+
+    # sure background area
+    sure_bg = cv2.dilate(opening,kernel,iterations=3)
+
+    # Finding sure foreground area
+    dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
+    ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+
+    # Finding unknown region
+    sure_fg = np.uint8(sure_fg)
+    unknown = cv2.subtract(sure_bg,sure_fg)
+
+    # Marker labelling
+    ret, markers = cv2.connectedComponents(sure_fg)
+
+    # Add one to all labels so that sure background is not 0, but 1
+    markers = markers+1
+
+    # Now, mark the region of unknown with zero
+    markers[unknown==255] = 0
+    markers = cv2.watershed(img,markers)
+    img[markers == -1] = [255,0,0]
+
+    im1 = cv2.cvtColor(img,cv2.COLOR_HSV2RGB)
+    cv2.imwrite("static/segmented.jpg", im1)
+
+    return render_template("segmentation.html")
 
 
 @app.after_request
